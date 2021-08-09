db1d3b1f797ec45dc6daa785343c6d2c134d0f8c
Ruud van engelenhoven <info@appthena.com>
2021-08-09 11:16:08 +0400

    Initial Commit

---
+++ kwote/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kwote
+//
+//  Created by Ruud van Engelenhoven on 09/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kwote/kwoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  kwoteApp.swift
+//  kwote
+//
+//  Created by Ruud van Engelenhoven on 09/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct kwoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
